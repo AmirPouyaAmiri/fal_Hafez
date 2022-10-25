229048de24b4355077a05b8c3f8327aa8de2dfc0
pouya <seyedamirpouyaamiri@gmail.com>
2022-10-25 18:30:36 +0330

    some changes

--- app/src/main/java/com/amiri/faleHafez/MainActivity.java
+++ app/src/main/java/com/amiri/faleHafez/MainActivity.java
@@ -3,15 +3,52 @@ package com.amiri.faleHafez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.amiri.faleHafez.dataBase.FalDatabase;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
-
+    String x = "";
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        Button clickButton = (Button) findViewById(R.id.buttonFal);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getRandomPoem();
+
+            }
+        });
+    }
+    public void getRandomPoem(){
+        TextView textView = findViewById(R.id.textViewPoem);
+        TextView txt2 = findViewById(R.id.textView2);
+
+
+        ArrayList<String> poems = new ArrayList<>();
+        poems.add("soso");
+        poems.add("sdfdsfadfs");
+        poems.add("kikieyrgdsjdf");
+        Collections.shuffle(poems);
+
+
+        for (String f: poems) {
+            if (!f.equals(x)) {
+                textView.setText(f);
+                x = f;
+            }else {
+                Collections.shuffle(poems);
+                textView.setText(f);
+            }
+        }
     }
 }
