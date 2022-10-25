d33a04cef2b5b5f0339fbbcbfa9002427c562efe
pouya <seyedamirpouyaamiri@gmail.com>
2022-10-25 14:40:26 +0330

    create DAO and DataBase for Fal

---
+++ app/src/main/java/com/amiri/faleHafez/dataBase/FalDatabase.java
@@ -0,0 +1,37 @@
+package com.amiri.faleHafez.dataBase;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.amiri.faleHafez.entity.Fal;
+import com.amiri.faleHafez.service.falDao;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Fal.class}, version = 1, exportSchema = false)
+public abstract class FalDatabase extends RoomDatabase {
+
+    public abstract falDao falDao();
+
+    private static volatile FalDatabase Instance;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static FalDatabase getDatabase(final Context context) {
+        if (Instance == null) {
+            synchronized (FalDatabase.class) {
+                if (Instance == null) {
+                    Instance = Room.databaseBuilder(context.getApplicationContext(),
+                            FalDatabase.class, "fal_database")
+                            .build();
+                }
+            }
+        }
+        return Instance;
+    }
+}
